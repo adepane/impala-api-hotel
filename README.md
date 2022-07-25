448532eb59e592f0042ece0aa8f75a217b20c390
adepane <adesbpane@gmail.com>
2022-07-25 21:25:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## About This App
+
+This app using laravel 9.3 and PHP 8.1.x only to make proxy request to Impala API, it is because they not
+allowed request on client. 
+
+
+## App Status (Experimental)
+
+This App is so experimental, I only using to complete my course at DTS2022
+
+## Usage
+
+Change the IMPALA_API on .env with yours.
+
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('{params}', function($params, Request $request) {
+    $response = Http::withHeaders([
+        'x-api-key' => config('app.impala_api'),
+    ])->get('https://sandbox.impala.travel/v1/'.$params."?".http_build_query($request->all()));
+    $theResponse = $response->getBody()->getContents();
+    $tempResponse = str_replace('https://sandbox.impala.travel/v1/', '/', $theResponse);
+    return json_decode($tempResponse, true);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
