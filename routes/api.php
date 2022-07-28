5be02301bc20f806c19d39cdda24201fbb023915
adepane <adesbpane@gmail.com>
2022-07-28 19:15:36 +0700

    update route proxy

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,21 @@ use Illuminate\Support\Facades\Http;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// get query by params like '/hotels' or '/bookings'
 Route::get('{params}', function($params, Request $request) {
     $response = Http::withHeaders([
         'x-api-key' => config('app.impala_api'),
     ])->get('https://sandbox.impala.travel/v1/'.$params."?".http_build_query($request->all()));
+    $theResponse = $response->getBody()->getContents();
+    $tempResponse = str_replace('https://sandbox.impala.travel/v1/', '/', $theResponse);
+    return json_decode($tempResponse, true);
+});
+
+// get query by id like '/hotels/1' or '/bookings/1'
+Route::get('/{params}/{id}', function($params, $id, Request $request) {
+    $response = Http::withHeaders([
+        'x-api-key' => config('app.impala_api'),
+    ])->get('https://sandbox.impala.travel/v1/'.$params."/".$id);
     $theResponse = $response->getBody()->getContents();
     $tempResponse = str_replace('https://sandbox.impala.travel/v1/', '/', $theResponse);
     return json_decode($tempResponse, true);
